3e896f213b9542d6545bf3f4427f88223f1951eb
leviathan-7 <m.grober02@yandex.ru>
2022-12-05 10:21:30 +0300

    factorial is made

---
+++ factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("factorial = " + factorial(a));
+        }
+        static int factorial(int a)
+        {
+            int s = 1;
+            for (int i = 2; i <= a; i++)
+                s *= i;
+            return s;
+        }
+    }
+}
